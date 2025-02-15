c614555edb5f5a9f66356f09cc337cd554f6cd38
naninani2xx1 <nhchuong2001@gmail.com>
2025-02-15 14:11:53 +0700

    [update]: load

--- src/NhcLoginBundle.php
+++ src/NhcLoginBundle.php
@@ -1,12 +1,25 @@
 <?php
 namespace Nhc\LoginBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class NhcLoginBundle extends Bundle
+class NhcLoginBundle extends AbstractBundle
 {
     public function getPath(): string
     {
         return dirname(__DIR__);
+    }
+
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+
+        $builder->setParameter('nhc.login.enabled', 'xinchao');
+
+        $container->parameters()->set('nhc.login.username', 'xinchao asd');
     }
 }
